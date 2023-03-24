cb9493c6a1fdaf33674cee18715f6778bb143a19
Maks <maksparadowski@gmail.com>
2023-03-24 12:15:30 +0100

    react helmet

--- frontend/src/views/signIn/SignIn.tsx
+++ frontend/src/views/signIn/SignIn.tsx
@@ -1,55 +1,56 @@
-import { useEffect } from 'react';
 import { Link, Button, Box, Paper, Stack, Typography } from '@mui/material';
+import { Helmet } from 'react-helmet';
 
 import { appRoutes } from 'routes/routes';
 
 const SignIn = () => {
-  useEffect(() => {
-    document.title = 'HR Dashboard - Sign In';
-  }, []);
-
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      sx={{ minHeight: '100vh' }}
-    >
-      <Paper
-        elevation={5}
-        sx={{ p: 2, display: { xs: 'contents', sm: 'block' } }}
+    <>
+      <Helmet>
+        <title>HR Dashboard - Sign In</title>
+      </Helmet>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        sx={{ minHeight: '100vh' }}
       >
-        <Typography
-          variant="h1"
-          align="center"
-          sx={{
-            fontSize: '2rem',
-            fontWeight: 'medium',
-            margin: '1rem',
-            wordBreak: 'break-all'
-          }}
+        <Paper
+          elevation={5}
+          sx={{ p: 2, display: { xs: 'contents', sm: 'block' } }}
         >
-          Sign in
-        </Typography>
-        <Stack
-          direction={{ xs: 'column', sm: 'row' }}
-          justifyContent="center"
-          alignItems="center"
-          spacing={1.5}
-        >
-          <Link href={appRoutes.main}>
-            <Button
-              color="primary"
-              variant="contained"
-              sx={{ fontSize: '1.2rem', m: 2, p: 2 }}
-            >
-              Back
-            </Button>
-          </Link>
-        </Stack>
-      </Paper>
-    </Box>
+          <Typography
+            variant="h1"
+            align="center"
+            sx={{
+              fontSize: '2rem',
+              fontWeight: 'medium',
+              margin: '1rem',
+              wordBreak: 'break-all'
+            }}
+          >
+            Sign in
+          </Typography>
+          <Stack
+            direction={{ xs: 'column', sm: 'row' }}
+            justifyContent="center"
+            alignItems="center"
+            spacing={1.5}
+          >
+            <Link href={appRoutes.main}>
+              <Button
+                color="primary"
+                variant="contained"
+                sx={{ fontSize: '1.2rem', m: 2, p: 2 }}
+              >
+                Back
+              </Button>
+            </Link>
+          </Stack>
+        </Paper>
+      </Box>
+    </>
   );
 };
 
--- frontend/src/views/singUp/SignUp.tsx
+++ frontend/src/views/singUp/SignUp.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import {
   Link,
   Box,
@@ -11,6 +11,7 @@ import {
 } from '@mui/material';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 import { appRoutes } from 'routes/routes';
 
@@ -51,10 +52,6 @@ const SignUp = () => {
       });
   };
 
-  useEffect(() => {
-    document.title = 'HR Dashboard - Sign Up';
-  }, []);
-
   const required = {
     value: true,
     message: 'This field cannot be empty'
@@ -69,137 +66,145 @@ const SignUp = () => {
   };
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      sx={{ minHeight: '100vh' }}
-    >
-      <Paper
-        elevation={5}
-        sx={{ p: 2, display: { xs: 'contents', sm: 'block', width: '400px' } }}
+    <>
+      <Helmet>
+        <title>HR Dashboard - Sign Up</title>
+      </Helmet>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        sx={{ minHeight: '100vh' }}
       >
-        <Typography
-          variant="h1"
-          align="center"
+        <Paper
+          elevation={5}
           sx={{
-            fontSize: '2rem',
-            fontWeight: 'medium',
-            margin: '1rem',
-            wordBreak: 'break-all'
+            p: 2,
+            display: { xs: 'contents', sm: 'block', width: '400px' }
           }}
         >
-          Sign up
-        </Typography>
-        <form onSubmit={handleSubmit(onSubmit)}>
+          <Typography
+            variant="h1"
+            align="center"
+            sx={{
+              fontSize: '2rem',
+              fontWeight: 'medium',
+              margin: '1rem',
+              wordBreak: 'break-all'
+            }}
+          >
+            Sign up
+          </Typography>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack
+              justifyContent="center"
+              alignItems="center"
+              flexDirection="column"
+              spacing={1.5}
+            >
+              <TextField
+                label="First name *"
+                variant="standard"
+                sx={{ width: { xs: '230px', sm: '300px' } }}
+                {...register('firstName', {
+                  required,
+                  minLength,
+                  maxLength
+                })}
+                error={Boolean(errors.firstName)}
+                helperText={errors?.firstName?.message}
+              />
+              <TextField
+                label="Last name *"
+                variant="standard"
+                sx={{ width: { xs: '230px', sm: '300px' } }}
+                {...register('lastName', {
+                  required,
+                  minLength,
+                  maxLength
+                })}
+                error={Boolean(errors.lastName)}
+                helperText={errors?.lastName?.message}
+              />
+              <TextField
+                label="Email *"
+                variant="standard"
+                sx={{ width: { xs: '230px', sm: '300px' } }}
+                {...register('email', {
+                  required,
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'Please use a valid e-mail address'
+                  }
+                })}
+                error={Boolean(errors.email)}
+                helperText={errors?.email?.message}
+              />
+              <TextField
+                label="Password *"
+                variant="standard"
+                type="password"
+                sx={{ width: { xs: '230px', sm: '300px' } }}
+                {...register('password', {
+                  required,
+                  minLength: {
+                    value: 5,
+                    message: 'Please use at least 5 characters'
+                  },
+                  maxLength: {
+                    value: 15,
+                    message: 'Please use less than 15 characters'
+                  }
+                })}
+                error={Boolean(errors.password)}
+                helperText={errors?.password?.message}
+              />
+              <TextField
+                label="Repeat Password *"
+                variant="standard"
+                type="password"
+                sx={{ width: { xs: '230px', sm: '300px' } }}
+                {...register('passwordRepeat', {
+                  required,
+                  validate: value =>
+                    value === watch('password') || 'The passwords do not match',
+                  minLength: {
+                    value: 5,
+                    message: 'Please use at least 5 characters'
+                  },
+                  maxLength: {
+                    value: 15,
+                    message: 'Please use less than 15 characters'
+                  }
+                })}
+                error={Boolean(errors.passwordRepeat)}
+                helperText={errors?.passwordRepeat?.message}
+              />
+              {apiResponse && <Alert severity="error">{apiResponse}</Alert>}
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{ fontSize: '1rem', p: 1.5 }}
+              >
+                Sign Up
+              </Button>
+            </Stack>
+          </form>
           <Stack
+            direction={{ xs: 'column', sm: 'row' }}
             justifyContent="center"
             alignItems="center"
-            flexDirection="column"
             spacing={1.5}
           >
-            <TextField
-              label="First name *"
-              variant="standard"
-              sx={{ width: { xs: '230px', sm: '300px' } }}
-              {...register('firstName', {
-                required,
-                minLength,
-                maxLength
-              })}
-              error={Boolean(errors.firstName)}
-              helperText={errors?.firstName?.message}
-            />
-            <TextField
-              label="Last name *"
-              variant="standard"
-              sx={{ width: { xs: '230px', sm: '300px' } }}
-              {...register('lastName', {
-                required,
-                minLength,
-                maxLength
-              })}
-              error={Boolean(errors.lastName)}
-              helperText={errors?.lastName?.message}
-            />
-            <TextField
-              label="Email *"
-              variant="standard"
-              sx={{ width: { xs: '230px', sm: '300px' } }}
-              {...register('email', {
-                required,
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: 'Please use a valid e-mail address'
-                }
-              })}
-              error={Boolean(errors.email)}
-              helperText={errors?.email?.message}
-            />
-            <TextField
-              label="Password *"
-              variant="standard"
-              type="password"
-              sx={{ width: { xs: '230px', sm: '300px' } }}
-              {...register('password', {
-                required,
-                minLength: {
-                  value: 5,
-                  message: 'Please use at least 5 characters'
-                },
-                maxLength: {
-                  value: 15,
-                  message: 'Please use less than 15 characters'
-                }
-              })}
-              error={Boolean(errors.password)}
-              helperText={errors?.password?.message}
-            />
-            <TextField
-              label="Repeat Password *"
-              variant="standard"
-              type="password"
-              sx={{ width: { xs: '230px', sm: '300px' } }}
-              {...register('passwordRepeat', {
-                required,
-                validate: value =>
-                  value === watch('password') || 'The passwords do not match',
-                minLength: {
-                  value: 5,
-                  message: 'Please use at least 5 characters'
-                },
-                maxLength: {
-                  value: 15,
-                  message: 'Please use less than 15 characters'
-                }
-              })}
-              error={Boolean(errors.passwordRepeat)}
-              helperText={errors?.passwordRepeat?.message}
-            />
-            {apiResponse && <Alert severity="error">{apiResponse}</Alert>}
-            <Button
-              type="submit"
-              variant="contained"
-              sx={{ fontSize: '1rem', p: 1.5 }}
-            >
-              Sign Up
-            </Button>
+            <Typography sx={{ mt: 2 }}>
+              Already have an account? Then{' '}
+              <Link href={appRoutes.signIn}>Sign In</Link>
+            </Typography>
           </Stack>
-        </form>
-        <Stack
-          direction={{ xs: 'column', sm: 'row' }}
-          justifyContent="center"
-          alignItems="center"
-          spacing={1.5}
-        >
-          <Typography sx={{ mt: 2 }}>
-            Already have an account? Then{' '}
-            <Link href={appRoutes.signIn}>Sign In</Link>
-          </Typography>
-        </Stack>
-      </Paper>
-    </Box>
+        </Paper>
+      </Box>
+    </>
   );
 };
 
