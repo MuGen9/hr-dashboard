01869c283d883f9e43867b687c80ae83834dd2e7
Maks <maksparadowski@gmail.com>
2023-03-27 12:22:16 +0200

    fixed errorMutate type

--- frontend/src/views/singUp/SignUp.tsx
+++ frontend/src/views/singUp/SignUp.tsx
@@ -14,12 +14,17 @@ import { useNavigate } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useMutation } from 'react-query';
+import { AxiosError } from 'axios';
 
 import { appRoutes } from 'routes/routes';
 
 import { registerRequest } from '../../api/api';
 
-import { registerSchema, SignUpForm } from './register.schema';
+import {
+  registerSchema,
+  SignUpForm,
+  SignUpRequestPayload
+} from './register.schema';
 import * as styles from './SignUp.styles';
 
 const SignUp = () => {
@@ -38,11 +43,14 @@ const SignUp = () => {
     mutate,
     isLoading,
     error: mutateError
-  } = useMutation(registerRequest, {
-    onSuccess: () => {
-      navigate(appRoutes.signIn);
+  } = useMutation<void, AxiosError<{ message: string }>, SignUpRequestPayload>(
+    registerRequest,
+    {
+      onSuccess: () => {
+        navigate(appRoutes.signIn);
+      }
     }
-  });
+  );
 
   const onSubmit: SubmitHandler<SignUpForm> = userData => {
     const { passwordRepeat, ...dataWithoutPasswordRepeat } = userData;
@@ -61,63 +69,61 @@ const SignUp = () => {
           </Typography>
           <form onSubmit={handleSubmit(onSubmit)}>
             <Stack spacing={1.5} sx={styles.stack}>
-              <>
-                <TextField
-                  label="First name *"
-                  variant="standard"
-                  sx={{ width: { xs: '230px', sm: '320px' } }}
-                  {...register('firstName')}
-                  error={Boolean(errors.firstName)}
-                  helperText={errors.firstName?.message}
-                />
-                <TextField
-                  label="Last name *"
-                  variant="standard"
-                  sx={{ width: { xs: '230px', sm: '320px' } }}
-                  {...register('lastName')}
-                  error={Boolean(errors.lastName)}
-                  helperText={errors.lastName?.message}
-                />
-                <TextField
-                  label="Email *"
-                  variant="standard"
-                  sx={{ width: { xs: '230px', sm: '320px' } }}
-                  {...register('email')}
-                  error={Boolean(errors.email)}
-                  helperText={errors.email?.message}
-                />
-                <TextField
-                  label="Password *"
-                  variant="standard"
-                  type="password"
-                  sx={{ width: { xs: '230px', sm: '320px' } }}
-                  {...register('password')}
-                  error={Boolean(errors.password)}
-                  helperText={errors.password?.message}
-                />
-                <TextField
-                  label="Repeat Password *"
-                  variant="standard"
-                  type="password"
-                  sx={{ width: { xs: '230px', sm: '320px' } }}
-                  {...register('passwordRepeat')}
-                  error={Boolean(errors.passwordRepeat)}
-                  helperText={errors.passwordRepeat?.message}
-                />
-                {mutateError.response?.data?.message && (
-                  <Alert severity="error">
-                    {mutateError.response.data.message}
-                  </Alert>
-                )}
-                <Button
-                  type="submit"
-                  variant="contained"
-                  sx={{ fontSize: '1rem', p: 1.5 }}
-                >
-                  Sign Up
-                </Button>
-                {isLoading && <CircularProgress />}
-              </>
+              <TextField
+                label="First name *"
+                variant="standard"
+                sx={{ width: { xs: '230px', sm: '320px' } }}
+                {...register('firstName')}
+                error={Boolean(errors.firstName)}
+                helperText={errors.firstName?.message}
+              />
+              <TextField
+                label="Last name *"
+                variant="standard"
+                sx={{ width: { xs: '230px', sm: '320px' } }}
+                {...register('lastName')}
+                error={Boolean(errors.lastName)}
+                helperText={errors.lastName?.message}
+              />
+              <TextField
+                label="Email *"
+                variant="standard"
+                sx={{ width: { xs: '230px', sm: '320px' } }}
+                {...register('email')}
+                error={Boolean(errors.email)}
+                helperText={errors.email?.message}
+              />
+              <TextField
+                label="Password *"
+                variant="standard"
+                type="password"
+                sx={{ width: { xs: '230px', sm: '320px' } }}
+                {...register('password')}
+                error={Boolean(errors.password)}
+                helperText={errors.password?.message}
+              />
+              <TextField
+                label="Repeat Password *"
+                variant="standard"
+                type="password"
+                sx={{ width: { xs: '230px', sm: '320px' } }}
+                {...register('passwordRepeat')}
+                error={Boolean(errors.passwordRepeat)}
+                helperText={errors.passwordRepeat?.message}
+              />
+              {mutateError?.response?.data?.message && (
+                <Alert severity="error">
+                  {mutateError.response.data.message}
+                </Alert>
+              )}
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{ fontSize: '1rem', p: 1.5 }}
+              >
+                Sign Up
+              </Button>
+              {isLoading && <CircularProgress />}
             </Stack>
           </form>
           <Typography sx={{ mt: 2, textAlign: 'center' }}>
